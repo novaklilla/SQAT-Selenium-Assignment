0e6162f006e5d44688cf52affb2760f975e2cf66
Novák Lilla <novaklilla0621@gmail.com>
2024-05-19 14:52:16 +0200

    Updated GoodReadsSeleniumTest class
    Updated the simple test case that checks if the page loads

--- testbed/tests/mytest/src/test/java/GoodReadsSeleniumTest.java
+++ testbed/tests/mytest/src/test/java/GoodReadsSeleniumTest.java
@@ -1,40 +1,46 @@
 import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
+import org.junit.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import java.net.URL;
 import java.net.MalformedURLException;
+import org.openqa.selenium.support.ui.*;
 
 public class GoodReadsSeleniumTest {
 
     private WebDriver driver;
+	private WebDriverWait wait;
 
     @Before
-    public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        try {
-            driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        driver.manage().window().maximize();
-    }
+	public void setup() {
+		ChromeOptions options = new ChromeOptions();
+		
+		try {
+			driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		this.driver.manage().window().maximize();
+		
+		this.wait = new WebDriverWait(driver, 100);
+	}
 
     @Test
     public void testLoadGoodreadsHomepage() {
         driver.get("https://www.goodreads.com/");
         String pageTitle = driver.getTitle();
-        assertTrue("Page title does not contain Goodreads", pageTitle.contains("Goodreads"));
+        assertTrue("Page title does contain Goodreads", pageTitle.contains("Goodreads"));
     }
 
     @After
-    public void close() {
-        if (driver != null) {
-            driver.quit();
+    public void close() 
+	{
+        if (this.driver != null) {
+            this.driver.quit();
         }
     }
 }
